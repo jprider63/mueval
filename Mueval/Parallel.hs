fb68e1eaab7eac9c258668ad7672354550887d81
James Parker <james@galois.com>
2019-09-03 09:47:28 -0400

    .

--- Mueval/Parallel.hs
+++ Mueval/Parallel.hs
@@ -11,18 +11,32 @@ import Mueval.Interpreter
 import Mueval.ArgsParse
 
 -- | Fork off a thread which will sleep and then kill off the specified thread.
-watchDog :: Int -> ThreadId -> IO ()
-watchDog tout tid = do _ <- installHandler sigXCPU
-                                          (CatchOnce
-                                           $ throwTo tid $ ErrorCall "Time limit exceeded.") Nothing
-                       _ <- forkIO $ do
-                                   threadDelay (tout * 700000)
-                                   -- Time's up. It's a good day to die.
-                                   throwTo tid (ErrorCall "Time limit exceeded")
-                                   killThread tid -- Die now, srsly.
-                                   error "Time expired"
-                       return () -- Never reached. Either we error out here
-                                 -- or the evaluation thread finishes.
+watchDog :: Int -> ThreadId -> ThreadId -> IO ThreadId
+watchDog tout mid cid = do
+    forkIO $ do
+                threadDelay (tout * 700000)
+                -- Time's up. It's a good day to die.
+                timeoutHandler mid cid
+    -- return () -- Never reached. Either we error out here
+    --           -- or the evaluation thread finishes.
+
+data TimeoutHandler = TimeoutHandler {
+      timeoutHandlerDone :: (MVar Bool)
+    , runTimeoutHandler :: IO ()
+    }
+
+timeoutHandler :: ThreadId -> ThreadId -> IO TimeoutHandler
+timeoutHandler mid cids = do
+    doneMVar <- newMVar False
+    return $ TimeoutHandler doneMVar $ do
+        -- Say we're done.
+        done <- swapMVar doneMVar True
+
+        -- Kill child if we're not done.
+        unless done $ do
+            throwTo mid (ErrorCall "Time limit exceeded") -- Notify main thread.
+            mapM_ killThread cids -- Die now, srsly.
+            -- error "Time expired"
 
 -- | A basic blocking operation.
 block :: (t -> MVar a -> IO t1) -> t -> IO a
@@ -52,7 +66,14 @@ forked' f timeLimit mvar = do mainId <- myThreadId
                               hSetBuffering stdout NoBuffering
 
                               -- Our modules and expression are set up. Let's do stuff.
-                              forkIO $ (f >>= putMVar mvar)
-                                         `E.catch` \e -> throwTo mainId (e::SomeException)
+                              childId <- forkIO $ (do
+                                    cid <- myThreadId
+                                    _ <- installHandler sigXCPU
+                                          (CatchOnce $ timeoutHandler mainId cid) Nothing
+                              
+                                    f >>= putMVar mvar
+                                ) `E.catch` \e -> throwTo mainId (e::SomeException)
                                                                 -- bounce exceptions to the main thread,
                                                                 -- so they are reliably printed out
+                              
+                              childId
